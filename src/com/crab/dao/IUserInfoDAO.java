278298c9bc3783bb6d1e42ad25229cc453f78e2f
wave12 <395702361@qq.com>
2017-01-24 23:14:18 +0800

    add user interface

---
+++ src/com/crab/dao/IUserInfoDAO.java
@@ -0,0 +1,16 @@
+package com.crab.dao;
+
+import java.util.List;
+
+import com.crab.entity.CbCart;
+import com.crab.entity.CbCartDetail;
+import com.crab.entity.CbUser;
+
+public interface IUserInfoDAO {
+    public int UserRegister(CbUser user);
+    public String UserExist(String userName);
+    public String UserLogin(String userName, String pass);
+    public CbCart GetCartInfoById(String id);
+    public List<CbCartDetail> GetCartDetailInfoByCartId(String cartId);
+    public boolean UpdateCartStatus(CbCart cart);
+}
