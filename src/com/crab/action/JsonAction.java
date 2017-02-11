408312a8d6a2e9c64530ecd38f8960d061131368
MCeDo <995011833@qq.com>
2017-02-11 15:33:49 +0800

    Add files via upload

--- src/com/crab/action/JsonAction.java
+++ src/com/crab/action/JsonAction.java
@@ -1,19 +1,13 @@
 package com.crab.action;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.struts2.interceptor.RequestAware;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.crab.biz.IAskProBiz;
-import com.crab.entity.CbDoc;
-import com.crab.entity.CbProduct;
-import com.crab.entity.CbSubCate;
-import com.crab.entity.CbUser;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class JsonAction extends ActionSupport
@@ -84,5 +78,29 @@ public class JsonAction extends ActionSupport
 		return SUCCESS;
 	}
 	
+	public String PayQuestion() {
+		String strRet = "";
+		try {
+			String[] payInfo = request.getParameter("payInfo").split("\\||;");
+			String userID    = payInfo[0];
+			String questionTitle = payInfo[1];
+			float payMon = Float.parseFloat(payInfo[2]);
+			//提问者余额足以支付
+			if(askProBiz.PayQuestionMon(userID, -payMon)) {
+				for (int i=3; i<payInfo.length; i+=2) {
+					//payInfo[i]为ID，payInfo[i+1]为报酬
+					//生成订单明细表
+					askProBiz.PayQuestion(userID, payInfo[i], questionTitle, Float.parseFloat(payInfo[i+1]));
+					//回答者余额增加
+					askProBiz.PayQuestionMon(payInfo[i], Float.parseFloat(payInfo[i+1]));
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			strRet = "0";
+		}
+		result = strRet;
+		return SUCCESS;
+	}
 	
 }
